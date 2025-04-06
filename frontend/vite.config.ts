1540c2788a1d35053d27f0a401fc5644eae33239
Lycoriste <steventeng03@gmail.com>
2025-04-05 19:58:18 -0700

    Frontend <-> Flask

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,21 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
+  build: {
+    assetsDir: 'static',
+  },
+  server: {
+    port: 3000,
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5555/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
